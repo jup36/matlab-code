fa69be10b1cff9f5b961e873a3a531b0fae712cb
Dohoung Kim <lapis42@gmail.com>
2017-04-01 12:45:16 -0400

    slideReg

--- functions/psth/slideReg.m
+++ functions/psth/slideReg.m
@@ -1,31 +1,26 @@
-function reg = slideReg(time, spk, predictor)
+function reg = slideReg(time, y, X, poisson)
+% poission: 0 - normal / 1 - poisson
 nBin = size(time,2);
-nVar = size(predictor,2);
+nVar = size(X, 2);
 
-p = zeros(nVar,nBin);
-src = zeros(nVar,nBin);
-sse = zeros(nVar,nBin);
-
-predStd = zeros(nVar,nBin);
-for iVar = 1:nVar
-    predStd(iVar,:) = std(predictor(:,iVar)) ./ std(spk,0,1);
-end
-
+[p, beta, ciDown, ciUp] = deal(zeros(nVar+1, nBin));
 for iBin = 1:nBin
-    [beta,~,stats] = glmfit(predictor, spk(:,iBin));
+    if poisson==1
+        mdl = fitglm(X, y(:, iBin), ...
+            'Distribution', 'poisson');
+    elseif poisson==0
+        mdl = fitglm(X, y(:, iBin), ...
+            'Distribution', 'normal');
+    end
     
-    src(:,iBin) = beta(2:end) .* predStd(:,iBin);
-    sse(:,iBin) = stats.se(2:end) .* predStd(:,iBin);
-    p(:,iBin) = stats.p(2:end);
+    beta(:, iBin) = mdl.Coefficients.Estimate;
+    p(:, iBin) = mdl.Coefficients.pValue;
+    ciTemp = coefCI(mdl);
+    ciDown(:, iBin) = ciTemp(:, 1);
+    ciUp(:, iBin) = ciTemp(:, 2);
 end
+timeci = [time flip(time)];
+ci = [ciDown flip(ciUp,2)];
 
-outRange = (abs(src+1.96*sse) > 10);
-src(outRange) = 0;
-sse(outRange) = 0;
-p(outRange) = 1;
-
-timesse = [time flip(time)];
-sse = [src-1.96*sse flip(src+1.96*sse,2)];
-
-reg = struct('time',time, 'p',p, 'src',src, 'timesse',timesse, 'sse',sse);
+reg = struct('time',time, 'p',p, 'beta', beta, 'timeci',timeci, 'ci',ci);
 end
